4dee25135998be2a8dca438076cf80eb0b0a40d2
sagar_varade <varade.sagar12@gmail.com>
2024-07-08 22:30:43 +0530

    Initial commit

---
+++ core_java/src/main/java/Pepconding/DSAONE/Basic/App.java
@@ -0,0 +1,14 @@
+package Pepconding.DSAONE.Basic;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class App {
+    @Test
+    public void demo() {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Hello World");
+        }
+    }
+}
