b15ce799a458ab1cda5697d94b3b0bf01d67fd2b
sagar_varade <varade.sagar12@gmail.com>
2024-08-04 17:51:45 +0530

    open api

--- JavaTechieSpring/src/main/java/com/JavaTechieSpring/controller/CourseController.java
+++ JavaTechieSpring/src/main/java/com/JavaTechieSpring/controller/CourseController.java
@@ -5,6 +5,8 @@ import com.JavaTechieSpring.DTO.CourseResponseDTO;
 import com.JavaTechieSpring.DTO.ServiceResponse;
 import com.JavaTechieSpring.service.CourseService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ import java.util.List;
 @RequestMapping("/course")
 public class CourseController {
 
+    Logger log= LoggerFactory.getLogger(CourseController.class);
     @Autowired  //Setter injection
     private CourseService courseService;
 
@@ -54,5 +57,15 @@ public class CourseController {
     @PutMapping("/{id}")
     public ServiceResponse<CourseResponseDTO> updateCourse(@PathVariable("id")  int id,@RequestBody @Valid CourseRequestDTO course ){
         return new ServiceResponse<CourseResponseDTO>(HttpStatus.OK,courseService.updateCourse(id,course));
+    }
+
+    @GetMapping("/log")
+    public String printLogger(){
+        log.trace("Trace message");
+        log.debug("Debug message");
+        log.info("Info message");
+        log.warn("WARN message");
+        log.error("Error message");
+        return "Log printed";
     }
 }
